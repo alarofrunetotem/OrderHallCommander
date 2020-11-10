79e943e2195b5fbc21f56c6744ac1e7241b5a157
Alar of Runetotem <alar@aspide.it>
2020-11-11 00:53:18 +0100

    Bumped

--- RelNotes.lua
+++ RelNotes.lua
@@ -14,7 +14,7 @@ self:Wiki([[
 == Silent mode ==
 typing /ohc silent in chat will eliminate every chat message from OrderHallCommander
 ]])
-self:RelNotes(1,8,3, [[
+self:RelNotes(1,8,4, [[
 Fix: Message: OrderHallCommander\libs\LibInit\LibInit-60.lua:46: unexpected symbol near ']'
 ]])
 self:RelNotes(1,8,2, [[
